1999417fc440d11c33a9d18ab240d001b7e6f5ed
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-03 09:48:36 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -7,8 +7,8 @@ int sum_range(int *start, int *end) {
 int main() {
   int data[] = {10, 20, 30, 40, 50, 60, 70};
 
-int result = sum_range(&data[1], &data[4]);
+  int result = sum_range(&data[1], &data[4]);
 
-printf("%d\n", result);
-return 0;
+  printf("%d\n", result);
+  return 0;
 }
