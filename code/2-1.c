9ea467bf5925e500c6e2ee4e1ad3174c5005347b
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-03 09:46:23 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -3,11 +3,11 @@
 int main() {
   char* alphas[] = {"abc", "def", "hij"};
 
-  int size = sizzeof(alphas) / sizeof(alphas[0]);
+  int size = sizeof(alphas) / sizeof(alphas[0]);
 
   for(int i = 0; i < size; i++) {
-  printf("%s\n", alphas[i]);
+    printf("%s\n", alphas[i]);
   }
 
-return 0;
+  return 0;
 }
