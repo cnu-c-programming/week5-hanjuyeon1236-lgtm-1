4d78795078774630c21f7466aaf2cb2af2c1fb97
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-03 09:29:01 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort_strings(char **arr, int size) {
+  char *temp;
+  for(int i = 0; i < size - 1; i++) {
+    for(int j = 0; j < size - 1 - i; j++) {
+      if {
+
+      }
+    }
+  }
+}
+
+int main() {
+  char* words[] = {"apple", "ant", "banana", "cherry"};
+  int size = 4;
+
+  sort_strings(words, size);
+
+  for(int i = 0; i < size; i++) {
+    printf("s\n", words[i]);
+  }
+
+  return 0;
+}
