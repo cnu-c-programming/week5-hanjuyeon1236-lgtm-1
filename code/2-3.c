435737ac47d135c29377a59cf91c8a9878894a0c
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-03 09:32:16 +0900

    Update 2-3.c

---
+++ code/2-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10, b = 20, c = 30, d = 40;
+  int *arr[] = {&a, &b, &c, &d};
+  int **pp = arr;
+
+  int **base = arr;
+
+  printf("%1d\n", (char *)pp - (char *)base);
+
+  pp = pp + 1;
+  printf("%d\n", (char *)pp - (char *)base);
+
+  pp = pp + 1;
+  printf("%1d\n", (char *)pp - (char *)base);
+
+  pp = pp + 1;
+  printf("%1d\n", (char *)pp - (char *)base);
+
+  return 0;
+}
