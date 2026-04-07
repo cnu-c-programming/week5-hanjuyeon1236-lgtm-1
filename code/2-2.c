f7594f5c6bbcd8f4008304564e4e1c500e6c5248
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-07 22:51:34 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -5,8 +5,10 @@ void sort_strings(char **arr, int size) {
   char *temp;
   for(int i = 0; i < size - 1; i++) {
     for(int j = 0; j < size - 1 - i; j++) {
-      if {
-
+      if (strcmp(arr[j], arr[j+1] > 0) {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
       }
     }
   }
